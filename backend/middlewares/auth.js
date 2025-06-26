8b9ae3eebd9f2875839f3d6394440c74ba727aea
Aadeep01 <aadeepkaur@yahoo.com>
2025-06-27 03:23:52 +0530

    Add user authentication check in authorization middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -25,6 +25,12 @@ exports.protect = async (req, res, next) => {
 
 exports.authorize = (...roles) => {
   return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        message: "User not authenticated. Please login first.",
+      });
+    }
+
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         message: `User role ${req.user.role} is not authorized to access this route`,
